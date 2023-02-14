58e9edc80b8adb4e464a75c0db0a4fdc1e6ffeb3
10994015 <a0938599191@gmail.com>
2023-02-15 05:03:42 +0800

    0215

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -32,7 +32,7 @@
     <script src="{{asset('js/app.js')}}"></script>
     <script src="{{asset('js/table2excel.js')}}"></script>
     <script src="{{asset('js/script.js')}}"></script>
-    
+    @stack('scripts')
     
 </body>
 </html>
--- resources/views/livewire/components/slidebar.blade.php
+++ resources/views/livewire/components/slidebar.blade.php
@@ -1,11 +1,15 @@
 <div class="slidebar">
     @if(Auth::user()->issub === 0)
     <ul>
-        <a href="/">儀錶板管理</a>
+        <a href="/">首頁</a>
         <a href="/proxy">代理管理</a>
         @if(Auth::user()->highest_auth) <a href="/subaccount">子帳號管理</a> @endif
         <a href="/member">會員管理</a>
-        <a href="/pointManage">上下分管理</a>
+        <a href="javascript:;" id="point">上下分管理<i class="fa-solid fa-chevron-down ml-2" id="point-down-icon"></i></a>
+        <ul class="child-link" id="point-child">
+            <a href="pointManage" >上分管理</a>
+            <a href="/">下分管理</a>
+        </ul>
         <a href="/gameManage">投注紀錄</a>
         <a href="/reportManage">營運報表</a>
     </ul>
@@ -19,4 +23,21 @@
         @if(DB::table('subaccount')->where('user_id', Auth::id())->first()->report === 1)<a href="/reportManage">營運報表</a> @endif
     </ul>
     @endif
+
+    <script>
+        const point = document.getElementById('point');
+        const pointDownIcon = document.getElementById('point-down-icon');
+        const pointChild = document.getElementById('point-child');
+        let pointChildIsOpen = false;
+        point.addEventListener('click', ()=>{
+            pointChildIsOpen = !pointChildIsOpen;
+            if(pointChildIsOpen){
+                pointChild.style.height = "90px";
+                pointDownIcon.style.transform = "rotate(180deg)";
+            }else{
+                pointChild.style.height = "0px";
+                pointDownIcon.style.transform = "rotate(0deg)";
+            }
+        })
+    </script>
 </div>
