a8784cfc2d942be213f06e16945be2a95cb35c6b
10994015 <a0938599191@gmail.com>
2023-03-01 04:27:15 +0800

    0301

--- app/Http/Livewire/Dashborad.php
+++ app/Http/Livewire/Dashborad.php
@@ -9,6 +9,8 @@ use Illuminate\Support\Facades\Log;
 use Livewire\Component;
 use Livewire\WithPagination;
 use App\Models\Subaccount;
+use Illuminate\Support\Facades\Auth;
+
 class Dashborad extends Component
 {
     use WithPagination;
@@ -18,15 +20,25 @@ class Dashborad extends Component
     public $total;
     public function mount(){
         for($i=0;$i<10;$i++){
-            $num = User::whereBetween('created_at', [(new Carbon())->subDays($i)->startOfDay()->toDateString(), (new Carbon())->subDays($i - 1)->startOfDay()->toDateString()])->where('utype', 'USR')->count();
+            if(Auth::user()->highest_auth){
+                $num = User::whereBetween('created_at', [(new Carbon())->subDays($i)->startOfDay()->toDateString(), (new Carbon())->subDays($i - 1)->startOfDay()->toDateString()])->where('utype', 'USR')->count();
+            }else{
+                $num = User::whereBetween('created_at', [(new Carbon())->subDays($i)->startOfDay()->toDateString(), (new Carbon())->subDays($i - 1)->startOfDay()->toDateString()])->where('utype', 'USR')->where('toponline', Auth::id())->count();
+            }
             array_push($this->dateArr, date("Y-m-d",strtotime("-$i day")));
             array_push($this->register_number, $num);
         }
         $this->dateArr = array_reverse($this->dateArr);
         $this->register_number = array_reverse($this->register_number);
 
-        $this->today = User::whereBetween('created_at', [(new Carbon())->subDays(0)->startOfDay()->toDateString(), (new Carbon())->subDays(-1)->startOfDay()->toDateString()])->where('utype', 'USR')->count();
-        $this->total = User::where('utype', 'USR')->count();
+        if(Auth::user()->highest_auth){
+            $this->today = User::whereBetween('created_at', [(new Carbon())->subDays(0)->startOfDay()->toDateString(), (new Carbon())->subDays(-1)->startOfDay()->toDateString()])->where('utype', 'USR')->count();
+            $this->total = User::where('utype', 'USR')->count();
+        }else{
+            $this->today = User::whereBetween('created_at', [(new Carbon())->subDays(0)->startOfDay()->toDateString(), (new Carbon())->subDays(-1)->startOfDay()->toDateString()])->where('utype', 'USR')->where('toponline', Auth::id())->count();
+            $this->total = User::where('utype', 'USR')->where('toponline', Auth::id())->count();
+        }
+        
     }
     public function hydrate(){
     }
